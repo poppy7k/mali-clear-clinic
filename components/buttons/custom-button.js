c2fd7f131f1bd0a4b5e07d320dcbd6cc9c0c99fc
Watcharavit Jiracheeppattana <watcharavit29@gmail.com>
2025-02-14 13:44:45 +0700

    fix: custom-button components

--- components/buttons/custom-button.js
+++ components/buttons/custom-button.js
@@ -14,6 +14,7 @@ class CustomButton extends HTMLElement {
         const id = this.getAttribute("id");
         const addClass = this.getAttribute("class");
         const bgColor = this.getAttribute("bgColor");
+        const type = this.getAttribute("type");
 
         // สร้างตัวแปรสำหรับเก็บ URL ของ SVG
         const iconUrl = `/mali-clear-clinic/assets/icons/${icon}.html`;
@@ -23,23 +24,23 @@ class CustomButton extends HTMLElement {
             fetch(iconUrl)
                 .then(response => response.text())
                 .then(svgContent => {
-                    this.renderButton(svgContent, text, color, href, hoverBg, hoverText, id, addClass, bgColor);
+                    this.renderButton(svgContent, text, color, href, hoverBg, hoverText, id, addClass, bgColor, type);
                 })
                 .catch(err => {
                     console.error("Error loading SVG:", err);
-                    this.renderButton('', text, color, href, hoverBg, hoverText, id, addClass, bgColor);
+                    this.renderButton('', text, color, href, hoverBg, hoverText, id, addClass, bgColor, type);
                 });
         } else {
-            this.renderButton('', text, color, href, hoverBg, hoverText, id, addClass, bgColor);
+            this.renderButton('', text, color, href, hoverBg, hoverText, id, addClass, bgColor, type);
         }
     }
 
-    renderButton(iconHtml, text, color, href, hoverBg, hoverText, id, addClass, bgColor) {
+    renderButton(iconHtml, text, color, href, hoverBg, hoverText, id, addClass, bgColor, type) {
         this.innerHTML = `
-            <a href="${href}" id="${id}" class="${addClass} bg-${bgColor} justify-center flex items-center text-${color} fill-${color} gap-2 py-2 px-4 rounded-xl text-sm font-semibold transition-all duration-300 hover:bg-${hoverBg} hover:text-${hoverText}">
+            <button type="${type}" href="${href}" id="${id}" class="${addClass} bg-${bgColor} justify-center flex items-center text-${color} fill-${color} gap-2 py-2 px-4 rounded-lg text-sm font-semibold transition-all duration-300 hover:bg-${hoverBg} hover:text-${hoverText}">
                 ${iconHtml ? `<span class="h-5 w-5">${iconHtml}</span>` : ''}
                 <span>${text}</span>
-            </a>
+            </button>
         `;
     }
 }
